0f17a42a6d554a2649b2451692aed43bb33aaab1
Alexey Rogachevskiy <sfalexrog@gmail.com>
2020-04-16 01:32:59 +0300

    gfx/ShaderSources: Downgrade to ESSL 1.0
    This should give us more compatibility with older devices.

--- gles_demos/src/gfx/ShaderSources.h
+++ gles_demos/src/gfx/ShaderSources.h
@@ -5,31 +5,29 @@ namespace Gfx
 namespace Shaders
 {
 const char undistortVertexShader[] = 
-"#version 300 es\n"
-"layout(location=0) in vec2 pos;\n"
-"layout(location=1) in vec2 texCoord;\n"
+"#version 100\n"
+"attribute vec2 pos;\n"
+"attribute vec2 texCoord;\n"
 "\n"
-"out vec2 vTexCoord;\n"
+"varying vec2 vTexCoord;\n"
 "\n"
 "void main()\n"
 "{\n"
 "  vTexCoord = texCoord;\n"
-"  gl_Position = vec4(pos.x, pos.y, 0.0, 1.0);\n"
+"  gl_Position = vec4(pos, 0.0, 1.0);\n"
 "}\n";
 
 // FIXME: This shader assumes the input texture has BGR format
 const char undistortFragmentShader[] = 
-"#version 300 es\n"
+"#version 100\n"
 "precision mediump float;\n"
 "uniform sampler2D tex;\n"
 "\n"
-"in vec2 vTexCoord;\n"
-"\n"
-"out vec4 Color;\n"
+"varying vec2 vTexCoord;\n"
 "\n"
 "void main()\n"
 "{\n"
-"  Color = texture(tex, vTexCoord).bgra;\n"
+"  gl_FragColor = texture2D(tex, vTexCoord).bgra;\n"
 "}\n";
 
 }
